9216874dfe0bf36e04ef6b6073a46e739ab3e428
maksim_antipov_1 <anti123456.antipov@yandex.ru>
2023-10-24 21:06:26 +0300

    init

---
+++ src/test/java/ru/araok/AraokConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.araok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AraokConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
